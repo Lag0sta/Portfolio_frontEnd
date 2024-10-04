c4fdb50b538bd1cb35012e70129aab0ffc087bd7
LRonnet <leo.ronnet@gmail.com>
2024-10-04 15:25:59 +0200

    Cv updated

--- components/TextGlitchAnimation.js
+++ components/TextGlitchAnimation.js
@@ -1,119 +1,122 @@
-import React, { useState, useEffect, useRef } from 'react';
+// import React, { useState, useEffect, useRef } from 'react';
+// import { titleAnimation } from './titleAnimation';
 
 
-function TextGlitchAnimation() {
+// function TextGlitchAnimation({text, element}) {
 
-  const [rectangles, setRectangles] = useState([]);
-  const containerRef = useRef(null);
+//   const [displayText, setDisplayText] = useState(text);
+//   const [rectangles, setRectangles] = useState([]);
+//   const containerRef = useRef(null);
 
-  useEffect(() => {
-    const intervalId = setInterval(generateRectangles, 75);
-    return () => clearInterval(intervalId);
-  }, []);
+//   const elementName = element.split(" '' ").join('')
 
-  const generateRectangles = () => {
-    const numRectangles = Math.floor(Math.random() * 6) + 7;
-    const containerWidth = containerRef.current ? containerRef.current.offsetWidth : 0;
-    const containerHeight = containerRef.current ? containerRef.current.offsetHeight : 0;
+//   useEffect(() => {
+//     const intervalId = setInterval(generateRectangles, 75);
+//     return () => clearInterval(intervalId);
+//   }, []);
 
-    const rectangles = Array(numRectangles).fill().map(() => {
-      const width = Math.random() * 90 + containerWidth / 20 + (Math.random() < 0.3 ? Math.random() * 150 : 0);
-      const height = Math.random() * 30 + containerHeight / 20 + (Math.random() < 0.3 ? Math.random() * 50 : 0);
-      const left = Math.floor(Math.random() * (containerWidth - width));
-      const top = Math.floor(Math.random() * (containerHeight - height));
-      return { width, height, left, top };
-    });
+//   useEffect(() => {
+//     if (typeof text === 'string') {
+//       titleAnimation(text, setDisplayText);
+//     } else {
+//       console.error('text prop must be a string');
+//     }  }, []);
 
-    setRectangles(rectangles);
-  };
+//   const generateRectangles = () => {
+//     const numRectangles = Math.floor(Math.random() * 6) + 7;
+//     const containerWidth = containerRef.current ? containerRef.current.offsetWidth : 0;
+//     const containerHeight = containerRef.current ? containerRef.current.offsetHeight : 0;
 
-  // const elementType = elementType1 || elementType2 || elementType3;
+//     const rectangles = Array(numRectangles).fill().map(() => {
+//       const width = Math.random() * 90 + containerWidth / 20 + (Math.random() < 0.3 ? Math.random() * 150 : 0);
+//       const height = Math.random() * 30 + containerHeight / 20 + (Math.random() < 0.3 ? Math.random() * 50 : 0);
+//       const left = Math.floor(Math.random() * (containerWidth - width));
+//       const top = Math.floor(Math.random() * (containerHeight - height));
+//       return { width, height, left, top };
+//     });
 
+//     setRectangles(rectangles);
+//   };
 
-  return (
-    <div>
-      <div ref={containerRef} style={{ position: 'relative', width: '50vh', height: '50vh', }}>
-        {rectangles.map((rectangle, index) => {
-          const newX = rectangle.left + (Math.random() - 0.5) * 0.01 * rectangle.width;
-          return (
-            <div style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              zIndex: 30,
-            }}>
-              <h2 style={{
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: '100%',
-                fontSize: 48, // add a font size to make the text visible
-                color: 'black', // add a text color to make the text visible
-                zIndex: -1,
-              }}
-              >
-                Glitchy Text
-              </h2>
-              <h2
-                key={index}
-                style={{
-                  position: 'absolute',
-                  top: -40,
-                  left: 0.5,
-                  width: '100%',
-                  height: '100%',
-                  clipPath: `inset(${rectangle.top}px ${containerRef.current.offsetWidth - newX - rectangle.width}px ${containerRef.current.offsetHeight - rectangle.top - rectangle.height}px ${newX}px)`,
-                  fontSize: 48, // add a font size to make the text visible
-                  color: 'black', // add a text color to make the text visible
-                  zIndex: 100000,
-                }}
-              >
-                Glitchy Text
-              </h2>
-              <h2
-                key={index}
-                style={{
-                  position: 'absolute',
-                  top: -40,
-                  left: -1.,
-                  width: '100%',
-                  height: '100%',
-                  clipPath: `inset(${rectangle.top}px ${containerRef.current.offsetWidth - newX - rectangle.width}px ${containerRef.current.offsetHeight - rectangle.top - rectangle.height}px ${newX}px)`,
-                  fontSize: 48, // add a font size to make the text visible
-                  color: 'cyan', // add a text color to make the text visible
-                  zIndex: 10,
-                }}
-              >
-                Glitchy Text
-              </h2>
-              <h2
-                key={index}
-                style={{
-                  position: 'absolute',
-                  top: -40,
-                  left: 2,
-                  width: '100%',
-                  height: '100%',
-                  clipPath: `inset(${rectangle.top}px ${containerRef.current.offsetWidth - newX - rectangle.width}px ${containerRef.current.offsetHeight - rectangle.top - rectangle.height}px ${newX}px)`,
-                  fontSize: 48, // add a font size to make the text visible
-                  color: 'magenta', // add a text color to make the text visible
-                  zIndex: 10,
-                }}
-              >
-                Glitchy Text
-              </h2>
-            </div>
-          );
-        })}
-      </div>
+//   return (
+//     <div>
+//       <div ref={containerRef} style={{ position: 'relative', width: '50vh', height: '50vh', }}>
+//         {rectangles.map((rectangle, index) => {
+//           const newX = rectangle.left + (Math.random() - 0.5) * 0.01 * rectangle.width;
+//           return (
+//             <div style={{
+//               position: 'absolute',
+//               top: 0,
+//               left: 0,
+//               width: '100%',
+//               height: '100%',
+//               zIndex: 30,
+//             }}>
+//               <elementName style={{
+//                 top: 0,
+//                 left: 0,
+//                 width: '100%',
+//                 height: '100%',
+//                 fontSize: 48,
+//                 color: 'black',
+//                 zIndex: -1,
+//               }}>
+//                 {text}
+//               </elementName>
+//               <elementName
+//                 key={index}
+//                 style={{
+//                   position: 'absolute',
+//                   top: -40,
+//                   left: 0.5,
+//                   width: '100%',
+//                   height: '100%',
+//                   clipPath: `inset(${rectangle.top}px ${containerRef.current.offsetWidth - newX - rectangle.width}px ${containerRef.current.offsetHeight - rectangle.top - rectangle.height}px ${newX}px)`,
+//                   fontSize: 48,
+//                   color: 'black',
+//                   zIndex: 100000,
+//                 }}
+//               >
+//                 {text}
+//               </elementName>
+//               <elementName
+//                 key={index}
+//                 style={{
+//                   position: 'absolute',
+//                   top: -40,
+//                   left: -1,
+//                   width: '100%',
+//                   height: '100%',
+//                   clipPath: `inset(${rectangle.top}px ${containerRef.current.offsetWidth - newX - rectangle.width}px ${containerRef.current.offsetHeight - rectangle.top - rectangle.height}px ${newX}px)`,
+//                   fontSize: 48,
+//                   color: 'cyan',
+//                   zIndex: 10,
+//                 }}
+//               >
+//                 {text}
+//               </elementName>
+//               <elementName
+//                 key={index}
+//                 style={{
+//                   position: 'absolute',
+//                   top: -40,
+//                   left: 2,
+//                   width: '100%',
+//                   height: '100%',
+//                   clipPath: `inset(${rectangle.top}px ${containerRef.current.offsetWidth - newX - rectangle.width}px ${containerRef.current.offsetHeight - rectangle.top - rectangle.height}px ${newX}px)`,
+//                   fontSize: 48,
+//                   color: 'magenta',
+//                   zIndex: 10,
+//                 }}
+//               >
+//                 {text}
+//               </elementName>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
 
-    </div>
-
-
-  );
-}
-
-
-export default TextGlitchAnimation;
+// export default TextGlitchAnimation;
--- components/titleAnimation.js
+++ components/titleAnimation.js
@@ -1,24 +1,25 @@
 
+
 export function titleAnimation(text, setRandomText) {
-    //Fonction permettant d'animer le titre par changements de lettres
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
-    let iteration = 0;
+  //Fonction permettant d'animer le titre par changements de lettres
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+  let iteration = 0;
 
-    const interval = setInterval(() => {
+  const interval = setInterval(() => {
 
-        const newText = text.split('').map((char, index) => {
-          if (index < iteration) {
-            return char;
-          } else {
-            return letters[Math.floor(Math.random() * letters.length)];
-          }
-        }).join('');
-        setRandomText(newText);
-        
-        if (iteration >= text.length) {
-          clearInterval(interval);
+      const newText = text.split('').map((char, index) => {
+        if (index < iteration) {
+          return char;
+        } else {
+          return letters[Math.floor(Math.random() * letters.length)];
         }
-        iteration += 1;
-        }, 85);
+      }).join('');
+      setRandomText(newText);
+      
+      if (iteration >= text.length) {
+        clearInterval(interval);
+      }
+      iteration += 1;
+      }, 85);
 
 }
